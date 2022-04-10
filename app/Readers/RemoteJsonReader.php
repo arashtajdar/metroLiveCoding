306c9eadcca3da389f6dc2002932d09cd4e5f1eb
arashtajdar <arashtajdar@gmail.com>
2022-04-10 11:59:55 +0200

    Create remote readers

---
+++ app/Readers/RemoteJsonReader.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace App\Readers;
+
+
+use App\Interfaces\OfferCollectionInterface;
+use App\Interfaces\ReaderInterface;
+
+class RemoteJsonReader implements ReaderInterface
+{
+
+    public function read(string $input): OfferCollectionInterface
+    {
+        // TODO: Implement read() method.
+    }
+}
---
+++ app/Readers/RemoteXmlReader.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Readers;
+
+use App\Interfaces\OfferCollectionInterface;
+use App\Interfaces\ReaderInterface;
+
+class XmlReader implements ReaderInterface
+{
+
+    public function read(string $input): OfferCollectionInterface
+    {
+        // TODO: Implement read() method.
+    }
+}
