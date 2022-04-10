2c8390aa8e4998f4d43cd30c7c06fb1ba7f59c31
arashtajdar <arashtajdar@gmail.com>
2022-04-10 15:16:31 +0200

    Initialize and add some codes to localJsonReader

--- app/Readers/localJsonReader.php
+++ app/Readers/localJsonReader.php
@@ -6,12 +6,38 @@ namespace App\Readers;
 
 use App\Interfaces\OfferCollectionInterface;
 use App\Interfaces\ReaderInterface;
+use App\Models\OfferCollection;
 
 class localJsonReader implements ReaderInterface
 {
-
+    public $offerCollection;
+    public function __construct(OfferCollectionInterface $offerCollection){
+        $this->offerCollection = $offerCollection;
+    }
     public function read(string $input): OfferCollectionInterface
     {
-        // TODO: Implement read() method.
+        $array = '[
+                        {
+                            "offerId": 123,
+                            "productTitle": "Coffee machine",
+                            "vendorId": 35,
+                            "price": 390.4
+                        },
+                        {
+                            "offerId": 124,
+                            "productTitle": "Napkins",
+                            "vendorId": 35,
+                            "price": 15.5
+                        },
+                        {
+                            "offerId": 125,
+                            "productTitle": "Chair",
+                            "vendorId": 84,
+                            "price": 333.0
+                        }
+                    ]';
+        $array = json_decode($array);
+        $this->offerCollection->setArray($array);
+        return $this->offerCollection;
     }
 }
