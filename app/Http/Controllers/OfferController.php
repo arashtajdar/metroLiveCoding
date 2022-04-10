2f2a62168bbf012fcd163c34afa9c74cd7dd74c8
arashtajdar <arashtajdar@gmail.com>
2022-04-10 15:43:13 +0200

    Add fake http request

--- app/Http/Controllers/OfferController.php
+++ app/Http/Controllers/OfferController.php
@@ -7,6 +7,7 @@ use App\Interfaces\OfferInterface;
 use App\Interfaces\ReaderInterface;
 use App\Readers\localJsonReader;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class OfferController extends Controller
 {
@@ -16,8 +17,29 @@ class OfferController extends Controller
 }
     public function countByPriceRange(float $price_from,float $priceTo): int
     {
-
-        $iterator = $this->reader->read("u")->getArray();
+//        $response = Http::get("https://api.publicapis.org/entries");
+//        $arrayString = json_encode($response->json());
+        $arrayString = '[
+                        {
+                            "offerId": 123,
+                            "productTitle": "Coffee machine",
+                            "vendorId": 35,
+                            "price": 390.4
+                        },
+                        {
+                            "offerId": 124,
+                            "productTitle": "Napkins",
+                            "vendorId": 35,
+                            "price": 15.5
+                        },
+                        {
+                            "offerId": 125,
+                            "productTitle": "Chair",
+                            "vendorId": 84,
+                            "price": 333.0
+                        }
+                    ]';
+        $iterator = $this->reader->read($arrayString)->getArray();
         $result = array_filter($iterator, function($val) use ($price_from,$priceTo) {
             return $val->price>$price_from && $val->price<$priceTo;
         });
--- app/Readers/localJsonReader.php
+++ app/Readers/localJsonReader.php
@@ -16,26 +16,7 @@ class localJsonReader implements ReaderInterface
     }
     public function read(string $input): OfferCollectionInterface
     {
-        $array = '[
-                        {
-                            "offerId": 123,
-                            "productTitle": "Coffee machine",
-                            "vendorId": 35,
-                            "price": 390.4
-                        },
-                        {
-                            "offerId": 124,
-                            "productTitle": "Napkins",
-                            "vendorId": 35,
-                            "price": 15.5
-                        },
-                        {
-                            "offerId": 125,
-                            "productTitle": "Chair",
-                            "vendorId": 84,
-                            "price": 333.0
-                        }
-                    ]';
+        $array = $input;
         $array = json_decode($array);
         $this->offerCollection->setArray($array);
         return $this->offerCollection;
